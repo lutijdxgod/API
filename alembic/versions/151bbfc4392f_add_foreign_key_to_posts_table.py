47b763c683deb87d0a30d31418b31457e988f70d
lutijdxgod <den31804@mail.ru>
2023-10-15 11:11:46 +0300

    fixed foreign key stuff

--- alembic/versions/151bbfc4392f_add_foreign_key_to_posts_table.py
+++ alembic/versions/151bbfc4392f_add_foreign_key_to_posts_table.py
@@ -10,17 +10,24 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '151bbfc4392f'
-down_revision = '842c049116ba'
+revision = "151bbfc4392f"
+down_revision = "842c049116ba"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
     op.add_column("posts", sa.Column("owner_id", sa.Integer, nullable=False))
-    op.create_foreign_key("posts_users_fkey", source_table="posts", referent_table="users", local_cols=["owner_id"], remote_cols=["id"], ondelete="CASCADE")
+    op.create_foreign_key(
+        "posts_owner_id_fkey",
+        source_table="posts",
+        referent_table="users",
+        local_cols=["owner_id"],
+        remote_cols=["id"],
+        ondelete="CASCADE",
+    )
 
 
 def downgrade() -> None:
-    op.drop_constraint("posts_user_fkey", table_name="posts")
+    op.drop_constraint("posts_owner_id_fkey", table_name="posts")
     op.drop_column("posts", "owner_id")
